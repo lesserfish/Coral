299458243eff1bfe4250b36c080c613522afb2fa
lesserfish <lesserfish@pm.me>
2024-06-14 14:06:51 -0300

    Minor

--- src/coral/controller.rs
+++ src/coral/controller.rs
@@ -1,5 +1,3 @@
-use crate::utils;
-
 pub struct Controller {
     pub state_data : u8,
     pub live_data : u8
@@ -18,8 +16,12 @@ impl Controller{
     pub fn write(&mut self){
         self.state_data = self.live_data;
     }
-    pub fn write_live(&mut self, byte : u8){
-        self.live_data = byte;
+    pub fn write_live(&mut self, state : u8){
+        self.live_data = state;
     }
 
+}
+
+pub fn new() -> Controller {
+    Controller { state_data: 0, live_data: 0 }
 }
