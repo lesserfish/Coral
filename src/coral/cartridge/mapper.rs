76b345c3e7efb52538429a0659ea71a5b7ed1f59
lesserfish <lesserfish@pm.me>
2024-06-13 22:08:30 -0300

    Implemented the API for Cartridge
    We implemented the simple cpu read cpu/ppu read/write/peek

--- src/coral/cartridge/mapper.rs
+++ src/coral/cartridge/mapper.rs
@@ -7,7 +7,6 @@ use std::io::ErrorKind;
 
 use crate::coral::cartridge as Cartridge;
 
-
 pub fn choose_mapper(cartridge : &mut Cartridge::Cartridge) -> io::Result<()>{
     match cartridge.header.h_mapper {
         0 => { 
@@ -22,4 +21,25 @@ pub fn choose_mapper(cartridge : &mut Cartridge::Cartridge) -> io::Result<()>{
 
 pub fn generic_mapper() -> types::Mapper {
     types::Mapper{0 : Box::new(mapper0::new(0, 0))}
+}
+
+impl types::Mapper {
+    pub fn cpu_r_map(&mut self, address : u16) -> u16{
+        self.0.cpu_r_mapt(address)
+    }
+    pub fn cpu_w_map(&mut self, address : u16) -> u16{
+        self.0.cpu_w_mapt(address)
+    }
+    pub fn cpu_p_map(&mut self, address : u16) -> u16{
+        self.0.cpu_p_mapt(address)
+    }
+    pub fn ppu_r_map(&mut self, address : u16) -> u16{
+        self.0.ppu_r_mapt(address)
+    }
+    pub fn ppu_w_map(&mut self, address : u16) -> u16{
+        self.0.ppu_w_mapt(address)
+    }
+    pub fn ppu_p_map(&mut self, address : u16) -> u16{
+        self.0.ppu_p_mapt(address)
+    }
 }
--- src/coral/cartridge/mapper/types.rs
+++ src/coral/cartridge/mapper/types.rs
@@ -10,23 +10,4 @@ pub trait MapperT {
 
 pub struct Mapper(pub Box<dyn MapperT>);
 
-impl Mapper {
-    fn cpu_r_map(&mut self, address : u16) -> u16{
-        self.0.cpu_r_mapt(address)
-    }
-    fn cpu_w_map(&mut self, address : u16) -> u16{
-        self.0.cpu_w_mapt(address)
-    }
-    fn cpu_p_map(&mut self, address : u16) -> u16{
-        self.0.cpu_p_mapt(address)
-    }
-    fn ppu_r_map(&mut self, address : u16) -> u16{
-        self.0.ppu_r_mapt(address)
-    }
-    fn ppu_w_map(&mut self, address : u16) -> u16{
-        self.0.ppu_w_mapt(address)
-    }
-    fn ppu_p_map(&mut self, address : u16) -> u16{
-        self.0.ppu_p_mapt(address)
-    }
-}
+
