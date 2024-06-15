6ecb00f7580cdc96bec82f9b3b02b8a24b4802aa
lesserfish <lesserfish@pm.me>
2024-06-15 13:31:17 -0300

    It's not working lmao

--- src/coral/bus/api.rs
+++ src/coral/bus/api.rs
@@ -13,8 +13,54 @@ impl Bus {
     pub fn set_controller_b(&mut self, state : u8){
         self.controller_b.write_live(state);
     }
-    pub fn tick(&mut self){}
-    pub fn frame(&mut self){}
+
+    fn tick_dma_r(&mut self, offset : u16){
+        let page = self.context.dma_page as u16;
+        let address = 0x100 * page + offset;
+        let byte = mos::Bus::read_byte(self, address);
+        self.context.dma_byte = byte;
+        self.context.dma_cycle += 1;
+    }
+    fn tick_dma_w(&mut self, offset : u16){
+        let byte = self.context.dma_byte;
+        ppu::dma_port(self, offset, byte);
+        self.context.dma_cycle += 1;
+        if offset == 255 {
+            self.context.dma_hold = false;
+        }
+    }
+    fn tick_dma(&mut self){
+        let dma_cycle = self.context.dma_cycle;
+        let offset = dma_cycle / 2;
+        if offset % 2 == 0 {
+            self.tick_dma_r(offset as u16);
+        } else {
+            self.tick_dma_w(offset as u16);
+        }
+    }
+    fn tick_cpu(&mut self){
+        let dma_hold = self.context.dma_hold; 
+        if dma_hold {
+            self.tick_dma();
+        } else {
+            mos::tick(self);
+        }
+    }
+    fn tick_ppu(&mut self){
+        ppu::tick(self);
+    }
+    pub fn tick(&mut self){
+        let clock = self.context.clock;
+        if clock % 3 == 0 {
+            self.tick_cpu();
+        }
+        self.tick_ppu();
+    }
+    pub fn frame(&mut self){
+        while !self.ppu.complete() {
+            self.tick();
+        }
+    }
     pub fn complete(&mut self){}
     pub fn get_pixel(&self, x : usize, y : usize) -> u8 {
         let address = y * 256 + x;
@@ -32,6 +78,6 @@ pub fn load<T : AsRef<Path>>(filepath : T) -> Result<Bus> {
     let controller_b = controller::new();
 
     let bus = Bus { context, cpu, ppu, cart, data, controller_a, controller_b };
-    
+
     Ok(bus)
 }
