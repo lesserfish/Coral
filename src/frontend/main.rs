82d09d7a27d32ca03adaaca37945fcad8ff82c3a
lesserfish <lesserfish@pm.me>
2024-10-07 07:12:55 +0000

    Now it works on MacOS
    Why the fuck does Mac not let me launch a fucking display from another
    process? Horseshit
    Dipshits.

--- src/frontend/main.rs
+++ src/frontend/main.rs
@@ -6,11 +6,8 @@ use std::thread;
 pub fn main(filepath : String) -> std::io::Result<()>{
     let (s1, s2)= shared::new();
 
-    let r = thread::spawn(move || {renderer::main(s1)});
     let e = thread::spawn(move || {emulator::main(filepath, s2)});
-
+    renderer::main(s1).unwrap();
     e.join().unwrap()?;
-    r.join().unwrap()?;
-
     Ok(())
 }
